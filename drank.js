483c6474948a9ee815ec33d47c4e2cc1f31e2548
KhaledaNYC <khaleda.khan87@gmail.com>
2016-09-01 14:55:17 -0400

    added merchant name phone rating logo drop down menu for alcohol

--- drank.js
+++ drank.js
@@ -6,6 +6,7 @@ $('button#get-dranks').click(function(){
   var addressUrl = address.split(' ').join("+");
   var zipcode = $('#zip-code').val();
   var alcohol = $('#alcohol').val();
+  debugger;
   var base_url = `https://www.delivery.com/api/data/search?search_type=alcohol&?section=${alcohol}&address=${addressUrl},${zipcode}&order_time=ASAP&order_type=delivery&client_id=brewhacks2016`
 
   $.ajax({
@@ -14,12 +15,15 @@ $('button#get-dranks').click(function(){
     success: function (response,status) {
       var merchantName = response.data.merchants[0]
       .summary.name
-      var merchantLogo = response.data.merchants[0].summary.merchant_logo_raw
+      var merchantLogo = response.data.merchants[0].summary.merchant_logo
       var phone = response.data.merchants[0].summary.phone
       var rating = response.data.merchants[0].summary.overall_rating
+      $('#merchant-label').text("Merchant: ")
       $('#merchant-name').text(merchantName);
       $('img#merchant-logo').attr('src', merchantLogo)
+        $('#phone-label').text("Phone: ")
       $('#merchant-phone').text(phone);
+        $('#rating-label').text("Rating: ")
       $('#merchant-rating').text(rating);
     }
   });
